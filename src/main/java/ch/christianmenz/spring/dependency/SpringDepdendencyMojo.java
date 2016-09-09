c357507a24bee45a94ce2b7b7b5184a2c108289e
Christian Menz <christianmenz@gmail.com>
2016-09-09 22:43:58 +0200

    load all dependencies into a custom classloader

--- src/main/java/ch/christianmenz/spring/dependency/SpringDepdendencyMojo.java
+++ src/main/java/ch/christianmenz/spring/dependency/SpringDepdendencyMojo.java
@@ -3,12 +3,20 @@ package ch.christianmenz.spring.dependency;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.io.Resources;
 import com.google.common.reflect.ClassPath;
+import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.nio.charset.Charset;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.model.Dependency;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -34,16 +42,25 @@ public class SpringDepdendencyMojo extends AbstractMojo {
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
         try {
-            final URL url = project.getArtifact().getFile().toURI().toURL();
-            System.out.println(url);
             
-            URLClassLoader classLoader = new URLClassLoader(new URL[] {url});
+            List<URL> urlList = project.getDependencyArtifacts().parallelStream().map(artifact -> toURL(artifact)).collect(Collectors.toList());
+            urlList.add(0, toURL(project.getArtifact()));
+                     
+            URLClassLoader classLoader = new URLClassLoader(urlList.toArray(new URL[0]));
             URL findResource = classLoader.findResource("application.xml");
             System.out.println(findResource);
         } catch (Exception e) {
             throw new MojoExecutionException("Failed", e);
         }
+    }
 
+    private URL toURL(Artifact artifact) {
+        try {
+            return artifact.getFile().toURI().toURL();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
