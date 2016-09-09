fcf8e9a1ac657347485a1b5eaada8208dfbc5572
Christian Menz <christianmenz@gmail.com>
2016-09-09 22:51:35 +0200

    setup resource recursion

--- src/main/java/ch/christianmenz/spring/dependency/SpringDepdendencyMojo.java
+++ src/main/java/ch/christianmenz/spring/dependency/SpringDepdendencyMojo.java
@@ -1,22 +1,14 @@
 package ch.christianmenz.spring.dependency;
 
-import com.google.common.collect.ImmutableSet;
 import com.google.common.io.Resources;
-import com.google.common.reflect.ClassPath;
-import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.net.URI;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.nio.charset.Charset;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import org.apache.maven.artifact.Artifact;
-import org.apache.maven.model.Dependency;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -41,14 +33,11 @@ public class SpringDepdendencyMojo extends AbstractMojo {
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        try {
-            
+        try {                        
             List<URL> urlList = project.getDependencyArtifacts().parallelStream().map(artifact -> toURL(artifact)).collect(Collectors.toList());
-            urlList.add(0, toURL(project.getArtifact()));
-                     
-            URLClassLoader classLoader = new URLClassLoader(urlList.toArray(new URL[0]));
-            URL findResource = classLoader.findResource("application.xml");
-            System.out.println(findResource);
+            urlList.add(0, toURL(project.getArtifact()));                     
+            URLClassLoader classLoader = new URLClassLoader(urlList.toArray(new URL[0]));           
+            printDependencies("application.xml", classLoader);
         } catch (Exception e) {
             throw new MojoExecutionException("Failed", e);
         }
@@ -63,4 +52,9 @@ public class SpringDepdendencyMojo extends AbstractMojo {
         return null;
     }
 
+    private void printDependencies(String resourcePath, URLClassLoader classLoader) throws IOException {
+        URL resourceUrl = classLoader.findResource(resourcePath);
+        String resourceContent = Resources.toString(resourceUrl, Charset.defaultCharset());
+        System.out.println(resourceContent);       
+    }
 }
