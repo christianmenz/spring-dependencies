5cbb2447e92bbb4d4cc1c5d70162c5e2c83317ab
Christian Menz <christianmenz@gmail.com>
2016-09-10 13:23:20 +0200

    implemented recursion to print resource loading

--- src/main/java/ch/christianmenz/spring/dependency/SpringDepdendencyMojo.java
+++ src/main/java/ch/christianmenz/spring/dependency/SpringDepdendencyMojo.java
@@ -1,13 +1,15 @@
 package ch.christianmenz.spring.dependency;
 
-import com.google.common.io.Resources;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.nio.charset.Charset;
 import java.util.List;
 import java.util.stream.Collectors;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import org.apache.commons.lang.StringUtils;
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -17,6 +19,9 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 
 /**
  *
@@ -31,13 +36,22 @@ public class SpringDepdendencyMojo extends AbstractMojo {
     @Parameter(defaultValue = "${project}", readonly = true, required = true)
     private MavenProject project;
 
+    @Parameter
+    private String applicationContext;
+    
+    @Parameter
+    private boolean printUrl;
+    
+    private XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
+
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        try {                        
+        try {
             List<URL> urlList = project.getDependencyArtifacts().parallelStream().map(artifact -> toURL(artifact)).collect(Collectors.toList());
-            urlList.add(0, toURL(project.getArtifact()));                     
-            URLClassLoader classLoader = new URLClassLoader(urlList.toArray(new URL[0]));           
-            printDependencies("application.xml", classLoader);
+            urlList.add(0, toURL(project.getArtifact()));
+            URLClassLoader classLoader = new URLClassLoader(urlList.toArray(new URL[0]));
+
+            printDependencies(applicationContext, classLoader, 0);
         } catch (Exception e) {
             throw new MojoExecutionException("Failed", e);
         }
@@ -52,9 +66,27 @@ public class SpringDepdendencyMojo extends AbstractMojo {
         return null;
     }
 
-    private void printDependencies(String resourcePath, URLClassLoader classLoader) throws IOException {
-        URL resourceUrl = classLoader.findResource(resourcePath);
-        String resourceContent = Resources.toString(resourceUrl, Charset.defaultCharset());
-        System.out.println(resourceContent);       
+    private void printDependencies(final String resourcePath, final URLClassLoader classLoader, int nestingDepth) throws IOException, XMLStreamException {
+        ResourceLoader loader = new DefaultResourceLoader(classLoader);
+        Resource resource = loader.getResource(resourcePath);        
+        
+        String depthIndicator = StringUtils.repeat(" ", nestingDepth);
+                
+        if (printUrl) {
+            System.out.println(depthIndicator + resource.getURL());
+        } else {
+            System.out.println(depthIndicator + resource.getFilename());
+        }
+        
+        nestingDepth += 1;
+        
+        XMLStreamReader xmlStreamReader = xmlInputFactory.createXMLStreamReader(resource.getInputStream());
+        while (xmlStreamReader.hasNext()) {
+            xmlStreamReader.next();
+            if (xmlStreamReader.isStartElement() && xmlStreamReader.getLocalName().equals("import")) {
+                String importResource = xmlStreamReader.getAttributeValue(null, "resource");                    
+                printDependencies(importResource, classLoader, nestingDepth);
+            }
+        }
     }
 }
