a0c056999ceebbd3cfafd1ecb15c5b26c1894395
Christian Menz <christianmenz@gmail.com>
2016-09-10 14:40:43 +0200

    implemented parsing configuration files myself

--- src/main/java/ch/christianmenz/spring/dependency/SpringDepdendencyMojo.java
+++ src/main/java/ch/christianmenz/spring/dependency/SpringDepdendencyMojo.java
@@ -1,11 +1,15 @@
 package ch.christianmenz.spring.dependency;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.List;
 import java.util.stream.Collectors;
 import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import org.apache.commons.lang.StringUtils;
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -15,6 +19,12 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternUtils;
 
 /**
  *
@@ -35,20 +45,21 @@ public class SpringDepdendencyMojo extends AbstractMojo {
     @Parameter
     private boolean printUrl;
 
+    private XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
+
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
         try {
             List<URL> urlList = project.getDependencyArtifacts().parallelStream().map(artifact -> toURL(artifact)).collect(Collectors.toList());
             urlList.add(0, toURL(project.getArtifact()));
-            URLClassLoader classLoader = new URLClassLoader(urlList.toArray(new URL[0]), getClass().getClassLoader());
-           
-            SpringDependencyApplicationContext context = new SpringDependencyApplicationContext(printUrl);
-            context.setClassLoader(classLoader);            
-            context.setConfigLocations(configLocations);
-            context.refresh(); // trigger config file loading
+            URLClassLoader classLoader = new URLClassLoader(urlList.toArray(new URL[0]));
+
+            for (String configLocation : configLocations) {
+                printDependencies(configLocation, classLoader, 0);
+            }
         } catch (Exception e) {
             e.printStackTrace();
-            throw new MojoExecutionException("Failed to process Spring configuration");
+            throw new MojoExecutionException("Failed", e);
         }
     }
 
@@ -59,5 +70,31 @@ public class SpringDepdendencyMojo extends AbstractMojo {
             e.printStackTrace();
         }
         return null;
+    }
+
+    private void printDependencies(final String resourcePath, final URLClassLoader classLoader, int nestingDepth) throws IOException, XMLStreamException {
+        ResourceLoader loader = new DefaultResourceLoader(classLoader);
+        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver(loader);        
+        Resource[] resources = resolver.getResources(resourcePath);
+        String depthIndicator = StringUtils.repeat("  ", nestingDepth);
+
+        for (Resource resource : resources) {
+            if (printUrl) {
+                System.out.println(depthIndicator + resource.getURL());
+            } else {
+                System.out.println(depthIndicator + resource.getFilename());
+            }
+
+            nestingDepth += 1;
+
+            XMLStreamReader xmlStreamReader = xmlInputFactory.createXMLStreamReader(resource.getInputStream());
+            while (xmlStreamReader.hasNext()) {
+                xmlStreamReader.next();
+                if (xmlStreamReader.isStartElement() && xmlStreamReader.getLocalName().equals("import")) {
+                    String importResource = xmlStreamReader.getAttributeValue(null, "resource");
+                    printDependencies(importResource, classLoader, nestingDepth);
+                }
+            }
+        }
     }
 }
